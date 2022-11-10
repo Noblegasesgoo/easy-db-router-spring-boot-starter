2c994dec85742129177b9edaae379f4a67122812
Noblegasesgoo <2632816823@qq.com>
2022-11-10 18:58:07 +0800

    新增常量

---
+++ src/main/java/com/zhao/middleware/db/router/common/Constant.java
@@ -0,0 +1,19 @@
+package com.zhao.middleware.db.router.common;
+
+/**
+ * @author: noblegasesgoo
+ * @CreateDate: 2022年11月10日 14:48
+ * @ProjectName: my-db-router-spring-boot-starter
+ * @version: 0.0.1
+ * @FileName: Constant
+ * @Description: 常量
+ */
+
+public class Constant {
+
+    public static final Integer ZERO = 0;
+
+    public static final Integer ONE = 1;
+
+    public static final Integer TWO = 2;
+}
